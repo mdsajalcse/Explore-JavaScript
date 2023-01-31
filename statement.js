5657c9328ee44d3f4a1d10f3ca2c26dccab35aa6
ifthekar <sajalcse2018@gmail.com>
2023-01-31 15:22:39 +0600

    statement.js added

---
+++ statement.js
@@ -0,0 +1,5 @@
+let a,b,c;
+a=10;
+b=3;
+c=a+b;
+console.log(c);
--- variable.js
+++ variable.js
@@ -2,3 +2,4 @@ var isFirstValue=10;
 var isSecondValue=90;
 var isSum=isFirstValue+isSecondValue;
 console.log(isSum);
+console.log(isSecondValue);
